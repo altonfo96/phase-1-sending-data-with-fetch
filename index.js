1dc2525426ba7ec99eaf9ecfd7a7c25696ae9804
Alton Fo <altonfo109@gmail.com>
2022-11-04 07:35:53 -0400

    commit tree 2d0b7519dcfcecacd99ad330c8424dee506cca30

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your code here
+const sumbitData = {
+    method:"POST",
+    headers: {
+        "Content-Type":'application/json',
+        "Accept":'application/json'
+    },
+    body:JSON.stringify({
+        name:"Alton",
+        Email:"afo@gmail.com"
+    })
+}
+fetch("http://localhost:3000/users",submitData);
